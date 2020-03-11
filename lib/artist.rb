9d129b6de024c5c0f6aeac1d49fc322d14e935f8
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-11 01:13:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
   attr_accessor :name 
-  
+  @@song_count = 0 
   def initialize(name)
     
     @name = name
@@ -11,11 +11,13 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    
+    @@song_count += 1 
   end
   
   def add_song_by_name(name)
-    
+    song = Song.new(name)
+    @songs << name 
+    @@song_count += 1 
   end 
   def songs
     @songs 
