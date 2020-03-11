cf7487b1a8ba7de527de9072ff387f6dfa43462c
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-11 01:38:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,5 +8,11 @@ class Author
     @@all << self 
   end
   def add_post(post)
-    
-    
+    post.author = self 
+  end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self 
+  end
+  
+end
